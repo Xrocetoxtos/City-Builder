e2b9482c22ab1f1a3817034c92df5bb883acc5c6
Xrocetoxtos <tommyvanson@gmail.com>
2024-08-31 23:14:34 +0200

    building type sprites

--- city/startup/sprites.lua
+++ city/startup/sprites.lua
@@ -2,5 +2,6 @@ local sprites = {}
 
     sprites.ui = {}
     sprites.ui.bonePointer = love.graphics.newImage("resources/sprites/ui/bone_pointer.png")
-
+    sprites.ui.building_types = love.graphics.newImage("resources/sprites/ui/BuildingTypes.png")
+    
 return sprites
