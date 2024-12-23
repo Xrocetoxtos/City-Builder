23f7e0e93ae6d25dd8ba3e3377481675f2c84c87
Xrocetoxtos <tommyvanson@gmail.com>
2024-12-23 17:36:48 +0100

    pickup animations

--- city/startup/animations.lua
+++ city/startup/animations.lua
@@ -7,10 +7,17 @@ local animations = {}
         animations.villager = {}
         local grid = Anim8.newGrid(16,16,Sprites.villager:getWidth(), Sprites.villager:getHeight())
         animations.villager.idle = Anim8.newAnimation(grid('1-2',1), 0.4)
+        
         animations.villager.walkLR = Anim8.newAnimation(grid('1-4',2), 0.2)
         animations.villager.walkRL = animations.villager.walkLR:clone()
         animations.villager.walkRL:flipH()
-        animations.villager.walkUD = Anim8.newAnimation(grid('1-4',3), 0.2)
+        animations.villager.walkUD = Anim8.newAnimation(grid('5-8',2), 0.2)
         animations.villager.walkDU = animations.villager.walkUD:clone()
+
+        animations.villager.pickLR = Anim8.newAnimation(grid('1-4',3), 0.2)
+        animations.villager.pickRL = animations.villager.walkLR:clone()
+        animations.villager.pickRL:flipH()
+        animations.villager.pickUD = Anim8.newAnimation(grid('5-8',3), 0.2)
+        animations.villager.pickDU = animations.villager.walkUD:clone()
 
 return animations
